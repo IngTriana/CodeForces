bff947996201873982eed4a335cfc7f63e5871a9
José Triana <jjtriana22@gmail.com>
2025-05-29 00:48:59 -0600

    Solucion al programa 282A

---
+++ 282A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _282A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int x = 0;
+            string V;
+            for (int i = 0; i < n; i++)
+            {
+                V = Console.ReadLine();
+                if(V == "++X" || V == "X++")
+                {
+                    x++;
+                }
+                else if (V == "--X" || V == "X--")
+                {
+                    x--;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
