079cb342245a93ba315e6f9b85022f79099dcadb
José Triana <jjtriana22@gmail.com>
2025-05-29 00:11:26 -0600

    Agregar archivos de proyecto.

---
+++ 581A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _581A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+            int a = int.Parse(entrada[0]);
+            int b = int.Parse(entrada[1]);
+
+            if (a == b)
+                Console.WriteLine(a + " 0");
+            else if (a > b)
+                Console.WriteLine(b + " " + ((a - b) / 2));
+            else
+                Console.WriteLine(a + " " + ((b - a) / 2));
+        }
+    }
+}
